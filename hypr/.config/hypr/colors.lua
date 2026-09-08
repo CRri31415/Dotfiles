f0787419e1e74d0b47578a7306ad7685b30dd2f1
CRri31415 <crri31415@gmail.com>
2026-09-08 11:46:11 +0900

    Add helix theme

--- hypr/.config/hypr/colors.lua
+++ hypr/.config/hypr/colors.lua
@@ -1,10 +1,10 @@
 local M = {
-	primary = "#92cef6",
+	primary = "#dcb9f8",
 	shadow = "#000000",
-	outline = "#8b9198",
-	outline_variant = "#41484d",
-	surface = "#101417",
-	on_primary = "#00344c",
+	outline = "#968e98",
+	outline_variant = "#4b454d",
+	surface = "#151217",
+	on_primary = "#3f2358",
 }
 
 return M
